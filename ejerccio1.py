8b30238cd0718070c1d6b1e25f239f346014e3f6
David <jhonatandavid9@gmail.com>
2024-01-20 08:58:10 -0500

    ejercicio1

---
+++ ejerccio1.py
@@ -0,0 +1,40 @@
+
+a = 5
+b= 10
+print("hola")
+
+print("el resultado de la suma es " , (a + b))
+print("la resta es " , a-b)
+
+print("la multiplicacion es " , a*b)
+print(type(a))
+
+
+
+#inputs (two static variables)
+num1 = 20 
+num2 = 5
+
+#process
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#outputs
+print("::: basic calc v1 :::")
+print("number 1:",num1,type(num1))
+print("number 2:",num2,type(num2))
+print("the addition is: ",add,type(add))
+print("the subtraction is: ",sub,type(sub))
+print("the multiplication is: ",mul,type(mul))
+print("the division is:", div,type(div))
+
+
+print("::: basic calc v1 :::")
+print("number 1:",num1,type(num1))
+print("number 2:",num2,type(num2))
+print("the addition is: " ,num1 + num2)
+print("the subtraction is: " ,num1 - num2)
+print("the multiplication is: ", num1 * num2)
+print("the division is:" ,num1 / num2)
